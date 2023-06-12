442a377d7c0a982d5e6d8318058b586ffc1fe860
Karolpanucci <karolainny.panucci@estudante.ifms.edu.br>
2023-06-12 16:50:51 -0400

    comit init

--- back end/models/produto.js
+++ back end/models/produto.js
@@ -1,20 +1,20 @@
 const db = require("../db");
 
-class Pessoa {
+class Produto {
   static async select() {
     try {
       const connect = await db.connect();
-      const sql = "SELECT *FROM pessoas"
+      const sql = "SELECT *FROM produtos"
       return await connect.query(sql);
     } catch (error) {
       console.error('Erro em select:', error);
       throw error;
     }
   }
-  static async selectOne(id) {
+  static async selectOne(codigo) {
     try {
       const connect = await db.connect();
-      const sql = "SELECT *FROM pessoas WHERE id=$1";
+      const sql = "SELECT *FROM produtos WHERE id=$1";
       return await connect.query(sql,[id]);
     } catch (error) {
       console.error('Erro em select:', error);
@@ -25,8 +25,8 @@ class Pessoa {
   static async insert(data) {
     try {
       const connect = await db.connect();
-      const sql = "INSERT INTO pessoas(nome, idade, uf) VALUES ($1, $2, $3) RETURNING id, nome, idade, uf;";
-      const values = [data.nome, data.idade, data.uf];
+      const sql = "INSERT INTO produto(codigo, titulo, dateCadastro, preco, descricao, imagen) VALUES ($1, $2, $3, $4, $5, $6) RETURNING codigo, titulo, dateCadastro, preco, descricao, imagen;";
+      const values = [data.codigo, data.titulo, data.dateCadastro, data.preco, data.descricao, data.imagen];
       return await connect.query(sql, values);
     } catch (error) {
       console.error('Erro em insert:', error);
@@ -46,11 +46,11 @@ class Pessoa {
     }
   }
 
-  static async delete(id) {
+  static async delete(codigo) {
     try {
       const connect = await db.connect();
-      const sql = "DELETE FROM pessoas WHERE id=$1";
-      return await connect.query(sql, [id]);
+      const sql = "DELETE FROM produto WHERE codigo=$1";
+      return await connect.query(sql, [codigo]);
     } catch (error) {
       console.error('Erro em delete:', error);
       throw error;
